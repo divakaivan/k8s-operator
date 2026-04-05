f98c7e8f7cf4a91696713eb94803de0abbeb1e0f
divakaivan <timee98@outlook.com>
2026-04-05 19:47:06 +0100

    missing import

--- internal/controller/ec2instance_controller.go
+++ internal/controller/ec2instance_controller.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	computev1 "github.com/divakaivan/operator-repo/api/v1"
